2d567e67bc26832060af72ea1177e943a790ded8
pmmoncada <pml350@nyu.edu>
2019-05-16 10:58:28 -0400

    map is now loading

---
+++ Data/infoDeptos.js
@@ -0,0 +1,128 @@
+[
+ {
+   "num": 1,
+   "departamento": "ATLANTIDA",
+   "2015migrantestotal": 1607,
+   "2015pop": 449822,
+   "2014unemplyment": 76206
+ },
+ {
+   "num": 2,
+   "departamento": "COLON",
+   "2015migrantestotal": 1788,
+   "2015pop": 319786,
+   "2014unemplyment": 71251
+ },
+ {
+   "num": 3,
+   "departamento": "COMAYAGUA",
+   "2015migrantestotal": 1163,
+   "2015pop": 511943,
+   "2014unemplyment": 99510
+ },
+ {
+   "num": 4,
+   "departamento": "COPAN",
+   "2015migrantestotal": 1187,
+   "2015pop": 382722,
+   "2014unemplyment": 78691
+ },
+ {
+   "num": 5,
+   "departamento": "CORTES",
+   "2015migrantestotal": 3611,
+   "2015pop": 1621762,
+   "2014unemplyment": 391560
+ },
+ {
+   "num": 6,
+   "departamento": "CHOLUTECA",
+   "2015migrantestotal": 946,
+   "2015pop": 447852,
+   "2014unemplyment": 119563
+ },
+ {
+   "num": 7,
+   "departamento": "EL PARAISO",
+   "2015migrantestotal": 424,
+   "2015pop": 458472,
+   "2014unemplyment": 121359
+ },
+ {
+   "num": 8,
+   "departamento": "FRANCISCO MORAZAN",
+   "2015migrantestotal": 2262,
+   "2015pop": 1553379,
+   "2014unemplyment": 392996
+ },
+ {
+   "num": 9,
+   "departamento": "GRACIAS A DIOS",
+   "2015migrantestotal": 31,
+   "2015pop": 94450,
+   "2014unemplyment": 0
+ },
+ {
+   "num": 10,
+   "departamento": "INTIBUCA",
+   "2015migrantestotal": 612,
+   "2015pop": 241568,
+   "2014unemplyment": 47138
+ },
+ {
+   "num": 11,
+   "departamento": "ISLAS DE LA BAHIA",
+   "2015migrantestotal": 162,
+   "2015pop": 65932,
+   "2014unemplyment": 0
+ },
+ {
+   "num": 12,
+   "departamento": "LA PAZ",
+   "2015migrantestotal": 208,
+   "2015pop": 206065,
+   "2014unemplyment": 52679
+ },
+ {
+   "num": 13,
+   "departamento": "LEMPIRA",
+   "2015migrantestotal": 704,
+   "2015pop": 333125,
+   "2014unemplyment": 63226
+ },
+ {
+   "num": 14,
+   "departamento": "OCOTEPEQUE",
+   "2015migrantestotal": 494,
+   "2015pop": 151516,
+   "2014unemplyment": 33322
+ },
+ {
+   "num": 15,
+   "departamento": "OLANCHO",
+   "2015migrantestotal": 2450,
+   "2015pop": 537306,
+   "2014unemplyment": 123223
+ },
+ {
+   "num": 16,
+   "departamento": "SANTA BARBARA",
+   "2015migrantestotal": 817,
+   "2015pop": 434896,
+   "2014unemplyment": 117948
+ },
+ {
+   "num": 17,
+   "departamento": "VALLE",
+   "2015migrantestotal": 661,
+   "2015pop": 178561,
+   "2014unemplyment": 43540
+ },
+ {
+   "num": 18,
+   "departamento": "YORO",
+   "2015migrantestotal": 1931,
+   "2015pop": 587375,
+   "2014unemplyment": 140598
+ }
+]
--- js/scripts.js
+++ js/scripts.js
@@ -10,75 +10,75 @@ var map = new mapboxgl.Map({
   zoom: 6,
 });
 
-var zoomThreshold = 4;
+// var zoomThreshold = 4;
+//
+// map.on('load', function() {
+//
+//     $.getJSON('Data/HNdeptos.geojson', function(data) {
+//       data.features.map(function(feature) {
+//         feature.properties.2015pop = parseInt(feature.properties.2015pop);
+//       });
+//
+//       data.features.forEach(function(feature) {
+//         console.log(feature.properties.2015pop)
+//       })
+//
 
-map.on('load', function() {
-
-    $.getJSON('Data/HNdeptos.geojson', function(data) {
-      data.features.map(function(feature) {
-        feature.properties.2015pop = parseInt(feature.properties.2015pop);
-      });
-
-      data.features.forEach(function(feature) {
-        console.log(feature.properties.2015pop)
-      })
-
-
-      map.addSource('HNdeptos', {
-          'type': 'geojson',
-          'data': data,
-      });
-
-      map.addLayer({
-        id: 'HNdeptos',
-        type: 'fill',
-        source: 'HNdeptos',
-        paint: {
-          'fill-opacity': 0.7,
-          'fill-color': [
-              'interpolate',
-              ['linear'],
-              ['get', 'pop2010'],
-              0, '#f1eef6',
-              50000, '#bdc9e1',
-              100000, '#74a9cf',
-              250000, '#2b8cbe',
-              500000, '#2b8cbe'
-          ],
-        }
-      });
-
-      // Create a popup, but don't add it to the map yet.
-      var popup = new mapboxgl.Popup({
-        closeButton: false,
-        closeOnClick: false
-      });
-
-      map.on('mouseenter', 'HNdeptos', function(e) {
-        // Change the cursor style as a UI indicator.
-        map.getCanvas().style.cursor = 'pointer';
-
-        var feature = e.features[0]
-
-        // Populate the popup and set its coordinates
-        // based on the feature found.
-        popup.setLngLat(e.lngLat)
-          .setHTML(`
-            <h4>${feature.properties.DEPTO}</h4><br/>
-            <p>Population: ${numeral(feature.properties.2015pop).format('0.0a')}</p>
-          `)
-          .addTo(map);
-      });
-
-      map.on('mouseleave', 'HNdeptos', function() {
-        map.getCanvas().style.cursor = '';
-        popup.remove();
-      });
-    });
-
-
-
-});
+//       map.addSource('HNdeptos', {
+//           'type': 'geojson',
+//           'data': data,
+//       });
+//
+//       map.addLayer({
+//         id: 'HNdeptos',
+//         type: 'fill',
+//         source: 'HNdeptos',
+//         paint: {
+//           'fill-opacity': 0.7,
+//           'fill-color': [
+//               'interpolate',
+//               ['linear'],
+//               ['get', 'pop2010'],
+//               0, '#f1eef6',
+//               50000, '#bdc9e1',
+//               100000, '#74a9cf',
+//               250000, '#2b8cbe',
+//               500000, '#2b8cbe'
+//           ],
+//         }
+//       });
+//
+//       // Create a popup, but don't add it to the map yet.
+//       var popup = new mapboxgl.Popup({
+//         closeButton: false,
+//         closeOnClick: false
+//       });
+//
+//       map.on('mouseenter', 'HNdeptos', function(e) {
+//         // Change the cursor style as a UI indicator.
+//         map.getCanvas().style.cursor = 'pointer';
+//
+//         var feature = e.features[0]
+//
+//         // Populate the popup and set its coordinates
+//         // based on the feature found.
+//         popup.setLngLat(e.lngLat)
+//           .setHTML(`
+//             <h4>${feature.properties.DEPTO}</h4><br/>
+//             <p>Population: ${numeral(feature.properties.2015pop).format('0.0a')}</p>
+//           `)
+//           .addTo(map);
+//       });
+//
+//       map.on('mouseleave', 'HNdeptos', function() {
+//         map.getCanvas().style.cursor = '';
+//         popup.remove();
+//       });
+//     });
+//
+//
+//
+// });
 
 
 
