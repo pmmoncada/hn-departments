e1b21e88c19aef3793984755ed73e83e20cc8168
pmmoncada <pml350@nyu.edu>
2019-05-16 13:42:20 -0400

    jquerry attempt

--- js/scripts.js
+++ js/scripts.js
@@ -10,81 +10,80 @@ var map = new mapboxgl.Map({
   zoom: 6.3,
 });
 
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
+
+
+
+
 // var zoomThreshold = 4;
 //
-// map.on('load', function() {
-//
-//     $.getJSON('Data/HNdeptos.geojson', function(data) {
-//       data.features.map(function(feature) {
-//         feature.properties.2015pop = parseInt(feature.properties.2015pop);
-//       });
-//
-//       data.features.forEach(function(feature) {
-//         console.log(feature.properties.2015pop)
-//       })
-//
+map.on('load', function() {
 
-//       map.addSource('HNdeptos', {
-//           'type': 'geojson',
-//           'data': data,
-//       });
-//
-//       map.addLayer({
-//         id: 'HNdeptos',
-//         type: 'fill',
-//         source: 'HNdeptos',
-//         paint: {
-//           'fill-opacity': 0.7,
-//           'fill-color': [
-//               'interpolate',
-//               ['linear'],
-//               ['get', 'pop2010'],
-//               0, '#f1eef6',
-//               50000, '#bdc9e1',
-//               100000, '#74a9cf',
-//               250000, '#2b8cbe',
-//               500000, '#2b8cbe'
-//           ],
-//         }
-//       });
-//
-//       // Create a popup, but don't add it to the map yet.
-//       var popup = new mapboxgl.Popup({
-//         closeButton: false,
-//         closeOnClick: false
-//       });
-//
-//       map.on('mouseenter', 'HNdeptos', function(e) {
-//         // Change the cursor style as a UI indicator.
-//         map.getCanvas().style.cursor = 'pointer';
-//
-//         var feature = e.features[0]
-//
-//         // Populate the popup and set its coordinates
-//         // based on the feature found.
-//         popup.setLngLat(e.lngLat)
-//           .setHTML(`
-//             <h4>${feature.properties.DEPTO}</h4><br/>
-//             <p>Population: ${numeral(feature.properties.2015pop).format('0.0a')}</p>
-//           `)
-//           .addTo(map);
-//       });
-//
-//       map.on('mouseleave', 'HNdeptos', function() {
-//         map.getCanvas().style.cursor = '';
-//         popup.remove();
-//       });
-//     });
-//
-//
-//
-// });
+    $.getJSON('Data/departmentsHN.geojson', function(data) {
+      data.features.map(function(feature) {
+        feature.properties.pop = parseInt(feature.properties.pop);
+      });
+
+      data.features.forEach(function(feature) {
+        console.log(feature.properties.pop)
+      })
 
 
+      map.addSource('HNdeptos', {
+          'type': 'geojson',
+          'data': './Data/departmentsHN.geojson',
+      });
+
+      map.addLayer({
+        id: 'HNdeptos',
+        type: 'fill',
+        source: 'Hndeptos',
+        paint: {
+          'fill-opacity': 0.7,
+          'fill-color': [
+              'interpolate',
+              ['linear'],
+              ['get', 'pop'],
+              0, '#f1eef6',
+              70000, '#bdc9e1',
+              100000, '#74a9cf',
+              250000, '#2b8cbe',
+              2000000, '#2b8cbe'
+          ],
+        }
+      });
+    });
+    });
 
 
-
-
+    //   // Create a popup, but don't add it to the map yet.
+    //   var popup = new mapboxgl.Popup({
+    //     closeButton: false,
+    //     closeOnClick: false
+    //   });
+    //
+    //   map.on('mouseenter', 'HNdeptos', function(e) {
+    //     // Change the cursor style as a UI indicator.
+    //     map.getCanvas().style.cursor = 'pointer';
+    //
+    //     var feature = e.features[0]
+    //
+    //     // Populate the popup and set its coordinates
+    //     // based on the feature found.
+    //     popup.setLngLat(e.lngLat)
+    //       .setHTML(`
+    //         <h4>${feature.properties.DEPTO}</h4><br/>
+    //         <p>Population: ${numeral(feature.properties.2015pop).format('0.0a')}</p>
+    //       `)
+    //       .addTo(map);
+    //   });
+    //
+    //   map.on('mouseleave', 'HNdeptos', function() {
+    //     map.getCanvas().style.cursor = '';
+    //     popup.remove();
+    //   });
+    // });
 // // use jquery to programmatically create a Legend
 // // for numbers 1 - 11, get the land use color and description
 // for (var i=1; i<18; i++) {
